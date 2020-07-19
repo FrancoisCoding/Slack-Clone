70b13bda1a78c4c00e36bf4f33b8854adeee3526
isaiah francois <francoisisaiaht@yahoo.com>
2020-07-19 15:02:54 -0400

    Created App Header and UserPanel Dropdown

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { Menu } from "semantic-ui-react";
+import UserPanel from "./UserPanel";
 
 class SidePanel extends React.Component {
   render() {
     return (
-      <div>
-        <h1>SidePanel</h1>
-      </div>
+      <Menu
+        size="large"
+        inverted
+        fixed="left"
+        vertical
+        style={{ background: "#4c3c4c", fontSize: "1.2rem" }}
+      >
+        <UserPanel />
+      </Menu>
     );
   }
 }
