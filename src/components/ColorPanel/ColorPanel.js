8b54deb2f4764de3af4affe8d1b158314d93bab3
isaiah francois <francoisisaiaht@yahoo.com>
2020-07-19 14:48:47 -0400

    Scaffolded app components

---
+++ src/components/ColorPanel/ColorPanel.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ColorPanel extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>ColorPanel</h1>
+      </div>
+    );
+  }
+}
+
+export default ColorPanel;
---
+++ src/components/Messages/Messages.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Messages extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Messages</h1>
+      </div>
+    );
+  }
+}
+
+export default Messages;
