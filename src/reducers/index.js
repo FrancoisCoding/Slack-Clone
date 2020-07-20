d2ef4b8ff3ff1adad4a26c76d54668e6e2ec35d2
isaiah francois <francoisisaiaht@yahoo.com>
2020-07-20 12:11:00 -0400

    Created channel actions and reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,12 @@
 import { combineReducers } from "redux";
 import * as actionTypes from "../actions/types";
 
-const initialState = {
+const initialUserState = {
   currentUser: null,
   isLoading: true,
 };
 
-const user_reducer = (state = initialState, action) => {
+const user_reducer = (state = initialUserState, action) => {
   switch (action.type) {
     case actionTypes.SET_USER:
       return {
@@ -15,8 +15,24 @@ const user_reducer = (state = initialState, action) => {
       };
     case actionTypes.CLEAR_USER:
       return {
-        ...initialState,
+        ...state,
         isLoading: false,
+      };
+    default:
+      return state;
+  }
+};
+
+const initialChannelState = {
+  currentChannel: null,
+};
+
+const channel_reducer = (state = initialChannelState, action) => {
+  switch (action.type) {
+    case actionTypes.SET_CURRENT_CHANNEL:
+      return {
+        ...state,
+        currentChannel: action.payload.currentChannel,
       };
     default:
       return state;
@@ -25,6 +41,7 @@ const user_reducer = (state = initialState, action) => {
 
 const rootReducer = combineReducers({
   user: user_reducer,
+  channel: channel_reducer,
 });
 
 export default rootReducer;
