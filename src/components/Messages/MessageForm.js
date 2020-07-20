62c96fba48880eccdd4bad89621b71260f5804c8
isaiah francois <francoisisaiaht@yahoo.com>
2020-07-20 14:27:02 -0400

    Created ProgressBar component for image upload

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuid } from "uuid";
 import firebase from "../../firebase";
 import { Segment, Button, Input } from "semantic-ui-react";
 
 import FileModal from "./FileModal";
+import ProgressBar from "./ProgressBar";
 
 class MessageForm extends React.Component {
   state = {
@@ -74,7 +75,7 @@ class MessageForm extends React.Component {
   uploadFile = (file, metadata) => {
     const pathToUpload = this.state.channel.id;
     const ref = this.props.messagesRef;
-    const filePath = `chat/public/${uuidv4()}.jpg`;
+    const filePath = `chat/public/${uuid()}.jpg`;
 
     this.setState(
       {
@@ -135,7 +136,8 @@ class MessageForm extends React.Component {
   };
 
   render() {
-    const { errors, message, loading, modal } = this.state;
+    // prettier-ignore
+    const { errors, message, loading, modal, uploadState, percentUploaded } = this.state;
 
     return (
       <Segment className="message__form">
@@ -170,12 +172,16 @@ class MessageForm extends React.Component {
             labelPosition="right"
             icon="cloud upload"
           />
-          <FileModal
-            modal={modal}
-            closeModal={this.closeModal}
-            uploadFile={this.uploadFile}
-          />
         </Button.Group>
+        <FileModal
+          modal={modal}
+          closeModal={this.closeModal}
+          uploadFile={this.uploadFile}
+        />
+        <ProgressBar
+          uploadState={uploadState}
+          percentUploaded={percentUploaded}
+        />
       </Segment>
     );
   }
